a1b2f8cfd387857b2eabca7e80a2e923f41e4b6a
Raj Patel <rajpatel4609@gmail.com>
2015-03-11 23:50:28 +0530

    [ADD] add partner_sequence_id module

---
+++ customer_id_sequence/customer_id_sequence.py
@@ -0,0 +1,31 @@
+
+from openerp.osv import fields, osv, orm
+from openerp.tools.translate import _
+
+
+class res_partner(osv.Model):
+    _inherit = 'res.partner'
+    _columns = {
+                'sequence':fields.char('Customer Id'),
+                }
+    _sql_constraints = [
+        ('uniq_reference', 'unique(sequence)', "The Customer Id must be unique"),
+        ]
+
+    def create(self, cr, uid, vals, context=None):
+        if context is None:
+            context = {}
+        if not 'sequence' in vals or vals['sequence'] == '/':
+            vals['sequence'] = self.pool.get('ir.sequence').get(
+                cr, uid, 'res.partner')
+        return super(res_partner, self).create(cr, uid, vals, context)
+
+    def copy(self, cr, uid, id, default={}, context=None):
+        product = self.read(cr, uid, id, ['sequence'], context=context)
+        if product['sequence']:
+            default.update({
+                'sequence': self.pool.get('ir.sequence').get(
+                cr, uid, 'res.partner')
+            })
+
+        return super(res_partner, self).copy(cr, uid, id, default, context)
